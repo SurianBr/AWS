3023a28ae5ec3c000794ff18c77dd4c30f419b18
SurianBr <rmsurian@hotmail.com>
2021-06-03 09:00:55 -0300

    adicionando --no-fail-on-empty-changeset no deploy do clodformation

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 for PIPE in $(jq '.pipes[].nome' -r pipelines.json)
 do
   echo valida template
-  cfn-lint "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml" --output json
+  cfn-lint "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml" --no-fail-on-empty-changeset --output json 
   if [ $? -ne 0 ]; then
     echo "Erro no lint"
     exit -1
