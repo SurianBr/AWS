b7db0214d3b0bf58684d1c3b25eba7cc07753fee
SurianBr <rmsurian@hotmail.com>
2021-06-03 08:55:23 -0300

    correção template dados de voos e ajustes no output do aws cli

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,10 +2,12 @@
 for PIPE in $(jq '.pipes[].nome' -r pipelines.json)
 do
   echo valida template
-  cfn-lint "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml"
+  cfn-lint "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml" --output json
   if [ $? -ne 0 ]; then
     echo "Erro no lint"
     exit -1
+  else
+    echo "Template ok"
   fi
 
   echo 'Fazendo deploy da pipe' $PIPE
