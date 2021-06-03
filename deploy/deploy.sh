ac6e7554ecbf6d3e6b6265a6b8d0e1081a7d05a5
SurianBr <rmsurian@hotmail.com>
2021-06-03 08:20:09 -0300

    correção do commando de deploy do aws cli no deply.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,5 @@
 for PIPE in $(jq '.pipes[].nome' -r pipelines.json)
 do
   echo 'Fazendo deploy da pipe' $PIPE
-  aws cloudformation --template-file "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml"
+  aws cloudformation deploy --template-file "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml"
 done
