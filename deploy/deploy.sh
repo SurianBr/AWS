b703ff1b2b12836d77e41494a02fb3e80e89ac4c
SurianBr <rmsurian@hotmail.com>
2021-06-03 08:44:40 -0300

    adicionando lint

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 for PIPE in $(jq '.pipes[].nome' -r pipelines.json)
 do
+  echo valida template
+  cfn-lint "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml"
+  if [ $retval -ne 0 ]; then
+    echo "Erro no lint"
+    exit -1
+  fi
+
   echo 'Fazendo deploy da pipe' $PIPE
   aws cloudformation deploy --template-file "${GITHUB_WORKSPACE}/${PIPE}/infra/template.yml" --stack-name ${PIPE}
 done
